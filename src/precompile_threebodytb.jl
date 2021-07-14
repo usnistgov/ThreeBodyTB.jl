ce016fc44c7cb1fedebc23172b752bc910711d5b
kfgarrity <kevin.garrity@nist.gov>
2021-07-14 14:17:30 -0400

    quick update lappy

--- src/precompile_threebodytb.jl
+++ src/precompile_threebodytb.jl
@@ -1,15 +1,23 @@
 using ThreeBodyTB
 
-
 #precompile all codes used in examples
 
-println("precompile")
+println("Precompiling, this may take a while...")
 
 #include(joinpath( ThreeBodyTB.TESTDIR ,"test_examples.jl"))
 
+try  #use Suppressor to avoid excessive output
 
-for t in ["calculate_energy_plot_fcc_al.jl", "load_dft_plot.jl", "load_tbc_plot.jl", "plot_Sc_P.jl", "relax_fcc_al.jl"]
-    include(joinpath( ThreeBodyTB.EXAMPLESDIR ,t))
-end
-
+    using Suppressor
+    @suppress for t in ["calculate_energy_plot_fcc_al.jl", "load_dft_plot.jl", "load_tbc_plot.jl", "plot_Sc_P.jl", "relax_fcc_al.jl"]
+        include(joinpath( ThreeBodyTB.EXAMPLESDIR ,t))
+    end
+    
+catch #Suppressor is missing
+    
+    for t in ["calculate_energy_plot_fcc_al.jl", "load_dft_plot.jl", "load_tbc_plot.jl", "plot_Sc_P.jl", "relax_fcc_al.jl"]
+        include(joinpath( ThreeBodyTB.EXAMPLESDIR ,t))
+    end
+    
+end    
 
