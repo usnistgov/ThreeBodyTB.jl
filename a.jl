f11e65957e1787f2208b1d4969b0f63cfafea455
kfgarrity <kevin.garrity@nist.gov>
2022-02-08 22:20:45 -0500

    quick update lappy

---
+++ a.jl
@@ -0,0 +1,40 @@
+### A Pluto.jl notebook ###
+# v0.18.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ a6104668-8955-11ec-2256-89a4ed7a01a5
+x = 1
+
+# ╔═╡ 1aaf0149-d6da-40b1-8119-fb43a726b1f9
+y = 2
+
+# ╔═╡ a17c5323-8b40-42d5-a16d-ddd685693830
+x + y
+
+# ╔═╡ 033940a6-ddf0-4e27-a3e8-9ba846c0defe
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═a6104668-8955-11ec-2256-89a4ed7a01a5
+# ╠═1aaf0149-d6da-40b1-8119-fb43a726b1f9
+# ╠═a17c5323-8b40-42d5-a16d-ddd685693830
+# ╠═033940a6-ddf0-4e27-a3e8-9ba846c0defe
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
