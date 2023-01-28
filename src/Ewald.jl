82f4a768a9be3397e8b4d3da6acf10702eb43953
kfgarrity <kevin.garrity@nist.gov>
2023-01-28 10:40:02 -0500

    quick update lappy

--- src/Ewald.jl
+++ src/Ewald.jl
@@ -24,7 +24,7 @@ using ..CrystalMod:crystal
 using SpecialFunctions
 import Base.Threads.@spawn
 using Base.Threads
-
+using LoopVectorization
 
 
 """
@@ -109,8 +109,7 @@ function electrostatics_getgamma(crys::crystal;  kappa=missing, noU=false, onlyU
     # can run real space and k-space in parallel with asyncronous parallelization. Only a minor improvement.
     #println("rs")
     rs = begin 
-    #rs = begin    
-        gamma_rs, gamma_U = real_space(crys, kappa, U, starting_size_rspace)
+        gamma_rs, gamma_U = real_space_LV(crys, kappa, U, starting_size_rspace)
     end
     
 #    println("gamma_rs")
@@ -118,7 +117,7 @@ function electrostatics_getgamma(crys::crystal;  kappa=missing, noU=false, onlyU
 #    println("ks")
     ks = begin 
     #ks = begin
-        gamma_k = k_space(crys, kappa, starting_size_kspace)
+        gamma_k = k_space_LV(crys, kappa, starting_size_kspace)
     end
 #    println("gamma_k")
 #    println(gamma_k)
@@ -330,6 +329,216 @@ function real_space(crys::crystal, kappa::Float64, U::Array{Float64}, starting_s
 end
 
 
+function real_space_LV(crys::crystal, kappa::Float64, U::Array{Float64}, starting_size_rspace=2)
+    
+    T = typeof(crys.coords[1,1])
+
+    first_iter = true
+    old_val = 0.0
+    
+    gamma_ij_tot = zeros(T, crys.nat, crys.nat)
+    gamma_ij_new = zeros(T, crys.nat, crys.nat)
+
+    gamma_U_tot = zeros(T, crys.nat, crys.nat)
+    gamma_U_new = zeros(T, crys.nat, crys.nat)
+ 
+    gamma_ij_new_th = zeros(T, crys.nat, crys.nat, nthreads())
+    gamma_U_new_th = zeros(T, crys.nat, crys.nat, nthreads())
+   
+    Uconst = zeros(T, crys.nat, crys.nat)
+
+    if sum(abs.(U)) > 1e-5
+        useU = true
+        for i = 1:crys.nat
+            Fi = sqrt( 8* log(2)/pi ) / (U[i]/2.0) #rydberg units, U in Ryd , we divide by 2, and multiply by e^2 layer. For Hartree formula, see koskinen comp mater sci 47 (2009) 237
+            for j = 1:crys.nat
+#                Uconst[i,j] = sqrt(pi/2 * (U[i]^2 * U[j]^2 / (U[i]^2 + U[j]^2)))
+
+                Fj = sqrt( 8* log(2)/pi ) / (U[j]/2.0)
+                Uconst[i,j] = sqrt(4 * log(2) / (Fi^2 + Fj^2))
+            end
+        end
+    else
+        useU = false
+    end
+
+    #    R = zeros(T,1, 3)
+    R = zeros(T, 3)
+    Ra = zeros(T, 3)
+    Rb = zeros(T, 3)
+    
+    coords_cart = crys.coords * crys.A
+    coords_cartT = coords_cart'
+
+    coords_cartTij = zeros(T, 3, crys.nat, crys.nat)
+    for i = 1:crys.nat
+        for j = 1:crys.nat
+            coords_cartTij[:,i,j] = (@view coords_cartT[:,i]) - (@view coords_cartT[:,j]) 
+        end
+    end
+    
+    
+    converged = false
+    converged_old = false
+    R0 = false
+
+    
+    N_list = [[1,1,1]]
+    a1 = sqrt(sum(crys.A[1,:].^2))
+    a2 = sqrt(sum(crys.A[2,:].^2))
+    a3 = sqrt(sum(crys.A[3,:].^2))
+    a = (minimum([a1,a2,a3]) - 0.0010) * 0.95
+
+#    println("a $a ", [a1, a2, a3])
+    
+    for N = 1:70
+
+        n1 = max(Int64(ceil( a * N / a1)), 2)
+        n2 = max(Int64(ceil( a * N / a2)), 2)
+        n3 = max(Int64(ceil( a * N / a3)), 2)
+
+        if [n1,n2,n3] != N_list[end]
+            N_list = push!(N_list, [n1,n2,n3])
+#            println("N_list ", (n1,n2,n3))
+        end
+    end
+
+    
+    newcontr = 0.0
+    newcontrU = 0.0
+    
+#    for N = starting_size_rspace:20
+#        Nx = N
+#        Ny = N
+#        Nz = N
+    Nxold = 0
+    Nyold = 0
+    Nzold = 0
+
+    At = crys.A'
+    
+    for (Nx, Ny, Nz) in N_list
+#        println("N ", [Nx, Ny, Nz])
+        gamma_ij_new[:,:] .= 0.0
+        gamma_U_new[:,:] .= 0.0
+
+        gamma_ij_new_th[:,:,:] .= 0.0
+        gamma_U_new_th[:,:,:] .= 0.0
+
+        for x = -Nx:Nx
+            Ra[1] = x
+            for y = -Ny:Ny
+                Ra[2] = y
+                for z = -Nz:Nz
+                    Ra[3] = z
+                    if x != 0 || y != 0 || z != 0
+                        R0 = false
+                    else
+                        R0 = true
+                    end
+                    R[:] .= At * Ra 
+                    if first_iter == true || (abs(x) > Nxold || abs(y) > Nyold || abs(z) > Nzold )
+                        if R0
+                            rs_core(crys, coords_cartTij, R, kappa, Uconst, gamma_ij_new, gamma_U_new, useU, R0)
+                        else 
+                            rs_core_R0false_thread(crys.nat, coords_cartTij, R, kappa, Uconst, gamma_ij_new_th, gamma_U_new_th, useU, R0)
+                        end
+
+                    end
+                end
+            end
+        end
+        gamma_U_new = gamma_U_new + sum(gamma_U_new_th, dims=3)[:,:]
+        gamma_ij_new = gamma_ij_new + sum(gamma_ij_new_th, dims=3)[:,:]
+
+        gamma_U_new = gamma_U_new + gamma_U_new' - diagm(diag(gamma_U_new))
+        gamma_ij_new = gamma_ij_new + gamma_ij_new' - diagm(diag(gamma_ij_new))
+        
+        newcontr = sum(abs.(gamma_ij_new))
+        newcontrU = sum(abs.(gamma_U_new))
+
+#        println("new $Nx $Ny $Nz real_space $newcontr $newcontrU")
+
+        if newcontr < 1e-5 && newcontrU < 1e-5
+            if converged_old == true || (newcontr < 1e-7 && newcontrU < 1e-7)
+                converged = true
+                break
+            end
+            converged_old = true
+        end
+        first_iter = false
+        
+        gamma_ij_tot += gamma_ij_new
+        gamma_U_tot += gamma_U_new
+
+        Nxold = Nx
+        Nyold = Ny
+        Nzold = Nz
+
+
+    end
+
+    if !(useU)
+        gamma_U_tot .= 0.0
+    end
+    
+    if converged == false
+        println("WARNING, real_space EWALD !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        println("real_space NOT converged  : $newcontr    $newcontrU")
+        println(crys)
+        
+    end
+
+    return gamma_ij_tot, gamma_U_tot
+
+end
+
+function rs_core(crys, coords_cartTij, R, kappa, Uconst, gamma_ij_new, gamma_U_new, useU, R0)
+
+    @inbounds @simd for i = 1:crys.nat
+        for j = i:crys.nat
+            Rb = (@view coords_cartTij[:,i, j])  - (@view R[:])
+            r = (Rb'*Rb).^0.5
+            if i != j ||  R0 == false
+                gamma_ij_new[i,j] += erfc( kappa * r) / r
+                gamma_U_new[i,j] += -erfc( Uconst[i,j] * r) / r  #see eq 3 in prb 66 075212, or koskinen comp mater sci 47 (2009) 237
+            end
+        end
+    end
+
+end
+
+function rs_core_R0false(nat, coords_cartTij, R, kappa, Uconst, gamma_ij_new, gamma_U_new, useU, R0)
+    @inbounds @simd for i = 1:nat
+        for j = i:nat
+           r=0.0
+           for a = 1:3
+               r += (coords_cartTij[a,i, j]  - R[a])^2
+           end
+           r=r^0.5
+           gamma_ij_new[i,j] += erfc( kappa * r) / r
+           gamma_U_new[i,j] += -erfc( Uconst[i,j] * r) / r  #see eq 3 in prb 66 075212, or koskinen comp mater sci 47 (2009) 237
+       end
+    end
+    
+end
+
+function rs_core_R0false_thread(nat, coords_cartTij, R, kappa, Uconst, gamma_ij_new, gamma_U_new, useU, R0)
+    @inbounds @threads for i = 1:nat
+        id = threadid()
+        for j = i:nat
+           r=0.0
+           for a = 1:3
+               r += (coords_cartTij[a,i, j]  - R[a])^2
+           end
+           r=r^0.5
+           gamma_ij_new[i,j,id] += erfc( kappa * r) / r
+           gamma_U_new[i,j,id] += -erfc( Uconst[i,j] * r) / r  #see eq 3 in prb 66 075212, or koskinen comp mater sci 47 (2009) 237
+       end
+    end
+    
+end
+
 
 """
     function estimate_best_kappa(A)
@@ -496,5 +705,132 @@ function k_space(crys::crystal, kappa, starting_size_kspace=2)
 end
 
 
+
+"""
+    function k_space(crys::crystal, kappa, starting_size_kspace=2)
+
+K-space Ewald sum.
+"""
+function k_space_LV(crys::crystal, kappa, starting_size_kspace=2)
+
+    T = typeof(crys.coords[1,1])
+
+    first_iter = true
+    old_val = 0.0
+    
+    gamma_ij_tot = zeros(T, crys.nat, crys.nat)
+    gamma_ij_new = zeros(T, crys.nat, crys.nat)
+    gamma_ij_new_th = zeros(T, crys.nat, crys.nat, nthreads())    
+    K = zeros(T, 1,3)
+    
+    coords_cart = crys.coords * crys.A
+    
+    converged = false
+    converged_old = false
+    
+    B = inv(crys.A)'
+    vol = abs(det(crys.A))
+
+    N_list = [[1,1,1]]
+    b1 = sqrt(sum(B[1,:].^2))
+    b2 = sqrt(sum(B[2,:].^2))
+    b3 = sqrt(sum(B[3,:].^2))
+    b = (minimum([b1,b2,b3]) - 0.0010) * 0.95
+
+    for N = 1:30
+
+        n1 = max(Int64(ceil( b * N / b1)), 2)
+        n2 = max(Int64(ceil( b * N / b2)), 2)
+        n3 = max(Int64(ceil( b * N / b3)), 2)
+
+        if [n1,n2,n3] != N_list[end]
+            N_list = push!(N_list, [n1,n2,n3])
+#            println("k list ", [n1,n2,n3])
+        end
+    end
+
+
+    newcontr = 0.0
+    
+    Nxold = 0
+    Nyold = 0
+    Nzold = 0
+
+    for (Nx, Ny, Nz) in N_list
+#        println("k ", [Nx, Ny, Nz])
+#    for N = starting_size_kspace:25
+        gamma_ij_new[:,:] .= 0.0
+        gamma_ij_new_th[:,:,:] .= 0.0
+        for kx = -Nx:Nx
+            for ky = -Ny:Ny
+                for kz = -Nz:Nz
+                    if kx == 0 && ky == 0 && kz == 0
+                        continue
+                    end
+                    K[1,:] .= [kx,ky,kz] 
+                    K = K * B
+                    k2 = sum(K.^2) * (2*pi)^2
+                    factor_k = (2*pi) * exp(-k2 / 4.0 / kappa^2  ) / k2
+
+                    if first_iter == true || abs(kx) > Nxold || abs(ky) > Nyold || abs(kz) > Nzold
+                        ks_core(crys, coords_cart, K, factor_k, gamma_ij_new_th)
+                    end
+                    
+                end
+            end
+        end
+        gamma_ij_new = gamma_ij_new + sum(gamma_ij_new_th, dims=3)[:,:]
+        
+        gamma_ij_new = gamma_ij_new + gamma_ij_new' - diagm(diag(gamma_ij_new))
+        first_iter = false
+        newcontr = sum(abs.(gamma_ij_new))
+
+        if newcontr < 1e-5
+            if converged_old == true || newcontr < 1e-7
+                converged = true
+                break
+            end
+            converged_old = true #need convergence twice
+
+        end
+
+        gamma_ij_tot += 2.0*gamma_ij_new /vol
+        
+        Nxold = Nx
+        Nyold = Ny
+        Nzold = Nz
+
+    end
+
+    if converged == false
+        println("WARNING, k_space EWALD !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        println("newcontr $newcontr")
+        println(crys)
+    end
+
+    return gamma_ij_tot
+
+end
+
+
+function ks_core(crys, coords_cart, K, factor_k, gamma_ij_new_th)
+
+    @inbounds @threads for i = 1:crys.nat
+        id = threadid()
+        for j = i:crys.nat
+            kr = 0.0
+            for a = 1:3
+                kr += K[a] * ( coords_cart[i,a] - coords_cart[j,a])
+            end
+                       
+            #            exp_c = exp(2*pi*im*kr)
+            #            temp = real(exp_c )
+            #gamma_ij_new_th[i,j,id] += factor_k * real(  exp(2*pi*im*kr)  )
+            gamma_ij_new_th[i,j,id] += factor_k * cos(2*pi*kr)
+            
+        end
+    end
+    
+end
 
 end #end module
